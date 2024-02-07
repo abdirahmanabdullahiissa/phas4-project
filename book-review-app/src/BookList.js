c679f564a7b6f691113637be9eeab094afe905e1
butch0012 <elnathanmburu@gmail.com>
2024-02-07 10:49:36 +0300

    data fetch

--- book-review-app/src/BookList.js
+++ book-review-app/src/BookList.js
@@ -2,4 +2,9 @@
 import React, { useState, useEffect } from 'react';
 const BookList = () => {
     const [books, setBooks] = useState([]);
+    try {
+        const response = await fetch('https://freetestapi.com/api/v1/books');
+        if (response.ok) {
+          const data = await response.json();
+          setBooks(data);
 
