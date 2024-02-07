f6efac5e4eff0b6def485c6786a4541a50df2b27
butch0012 <elnathanmburu@gmail.com>
2024-02-07 10:52:16 +0300

    error alert

--- book-review-app/src/BookList.js
+++ book-review-app/src/BookList.js
@@ -4,9 +4,13 @@ const BookList = () => {
     const [books, setBooks] = useState([]);
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
-    try {
-        const response = await fetch('https://freetestapi.com/api/v1/books');
-        if (response.ok) {
-          const data = await response.json();
-          setBooks(data);
+    useEffectseEffect(() => {
+        const fetchBooks = async () => {
+          try {
+            const response = await fetch('https://freetestapi.com/api/v1/books');
+            if (response.ok) {
+              const data = await response.json();
+              setBooks(data);
+            } else {
+                setError('Error fetching books');
 
