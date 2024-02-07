64510d7bbea2aa6fd2afbec66d4c3fc3ed2f0a6f
butch0012 <elnathanmburu@gmail.com>
2024-02-07 11:00:04 +0300

    import react

---
+++ book-review-app/src/SuggestBook.js
@@ -0,0 +1,6 @@
+// components/SubmitBook.js
+import React, { useState } from 'react';
+
+const SubmitBook = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
