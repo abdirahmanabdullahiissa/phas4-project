1c6c5155b2fb950d164efe354a22a158b9f0698d
butch0012 <elnathanmburu@gmail.com>
2024-02-07 10:29:55 +0300

    importation of components

---
+++ book-review-app/src/App.js
@@ -0,0 +1,10 @@
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import BookList from './components/BookList';
+import BookDetails from './components/BookDetails';
+import SubmitBook from './components/SubmitBook';
+import ReviewForm from './components/ReviewForm';
+import CommunityForum from './components/CommunityForum';
+import PostForm from './components/PostForm';
+
