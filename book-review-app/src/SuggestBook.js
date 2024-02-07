a7d20a31ad62998c0df9671e410415a97fc98403
butch0012 <elnathanmburu@gmail.com>
2024-02-07 11:01:19 +0300

    post data

--- book-review-app/src/SuggestBook.js
+++ book-review-app/src/SuggestBook.js
@@ -4,3 +4,13 @@ import React, { useState } from 'react';
 const SubmitBook = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('https://freetestapi.com/api/v1/books', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ title, author }),
+      });
